4c78cab92144ad723bc9574c4a0d786f6a664ca3
Mark Nemec <mrknmc@me.com>
2015-03-14 19:26:29 +0000

    probably should commit more often

---
+++ code/WordCountTopology.java
@@ -0,0 +1,11 @@
+public class WordCountTopology {
+    public static void main(String[] args) throws Exception {
+        TopologyBuilder builder = new TopologyBuilder();
+        builder.setSpout("spout", new RandomSentenceSpout(), 5);
+        builder.setBolt("split", new SplitSentence(), 8).shuffleGrouping("spout");
+        builder.setBolt("count", new WordCount(), 12).fieldsGrouping("split", new Fields("word"));
+
+        LocalCluster cluster = new LocalCluster();
+        cluster.submitTopology("word-count", conf, builder.createTopology());
+  }
+}
