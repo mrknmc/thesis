d9f082855cce3751baeccfdef7d1b389dbad7bc1
Mark Nemec <mrknmc@me.com>
2015-03-22 17:01:22 +0000

    How do I have so many pages... Finished Lit Review hopefully. Fixed referencing issues.

--- dump.R
+++ dump.R
@@ -2,6 +2,9 @@
 library("ggplot2")
 library("reshape2")
 
+# the color pallete
+cbPalette <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
 # read results
 dump <- read.csv(file="csv/dump.csv",sep=",")
 dump$TIMED_WAITING <- dump$TIMED_WAITING_monitor + dump$TIMED_WAITING_parking + dump$TIMED_WAITING_sleeping
@@ -12,5 +15,5 @@ dump$WAITING <- dump$WAITING_parking
 dump$WAITING_parking <- NULL
 dump$Time<-seq.int(nrow(dump))
 b <- melt(dump, id.vars="Time")
-k <- ggplot(b, aes(group=variable,y=value,x=Time,fill=variable)) + theme_bw() + scale_fill_grey()
-k + geom_area()
+k <- ggplot(b, aes(group=variable,y=value,x=Time,fill=variable)) + scale_fill_manual(name="Thread State", values=cbPalette)
+k + geom_area() + ylab("Number of threads")
