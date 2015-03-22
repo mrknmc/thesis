ce1dabd177f4ac1ddab09738dd493e36c3cf43c1
Mark Nemec <mrknmc@me.com>
2015-03-22 20:04:00 +0000

    kinda happy with lit review, bg and now apache storm on mc

--- dump.R
+++ dump.R
@@ -1,6 +1,9 @@
 # ---- dump-plot ----
 library("ggplot2")
 library("reshape2")
+library(scales)
+library(grid)
+library(RColorBrewer)
 
 # the color pallete
 cbPalette <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
@@ -15,5 +18,5 @@ dump$WAITING <- dump$WAITING_parking
 dump$WAITING_parking <- NULL
 dump$Time<-seq.int(nrow(dump))
 b <- melt(dump, id.vars="Time")
-k <- ggplot(b, aes(group=variable,y=value,x=Time,fill=variable)) + scale_fill_manual(name="Thread State", values=cbPalette)
-k + geom_area() + ylab("Number of threads")
+k <- ggplot(b, aes(group=variable,y=value,x=Time,fill=variable)) + scale_x_discrete(breaks=seq(0,180, by=30)) + scale_y_discrete(breaks=seq(0,60, by=10)) + scale_fill_manual(values=cbPalette,name="Thread State")
+k + geom_area() + ylab("# of threads") + geom_hline(yintercept=0, size=0.4, color="black")
